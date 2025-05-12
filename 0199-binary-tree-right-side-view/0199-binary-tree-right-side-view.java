be64528993c26f136302d2417a0007ee82ad6f12
SaiGovardhan <veggalam.saigovardhan@gmail.com>
2025-05-12 06:57:13 +0530

    Time: 1 ms (71.37%), Space: 42.1 MB (62.2%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,28 +1,42 @@
+import java.util.*;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        if (root != null) q.offer(root);
+        List<Integer> result = new ArrayList<>();
 
-        while (!q.isEmpty()) {
-            TreeNode rightSide = null;
-            int size = q.size();
+        if(root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int size = queue.size();
 
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                if (node != null) {
-                    rightSide = node;
-                    if (node.left != null) q.offer(node.left);
-                    if (node.right != null) q.offer(node.right);
-                }
+        while(!queue.isEmpty()){
+            TreeNode temp = null;
+            size = queue.size();
+
+            for(int i=0;i<size;i++){
+                temp = queue.poll();
+                if(temp.left !=null) queue.offer(temp.left);
+                if(temp.right !=null) queue.offer(temp.right);
             }
 
-            if (rightSide != null) {
-                res.add(rightSide.val);
-                System.out.println("adding to res -> " + rightSide.val);
+            if(temp!=null){
+                result.add(temp.val);
             }
         }
-
-        return res;        
+        return result;
     }
 }
