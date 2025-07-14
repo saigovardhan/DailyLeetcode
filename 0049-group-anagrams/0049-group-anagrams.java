46cb1542ec134478ba667d69dc731e12f9c7035d
SaiGovardhan <veggalam.saigovardhan@gmail.com>
2025-07-14 13:15:11 +0530

    Time: 18 ms (23.93%), Space: 49.5 MB (5.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,35 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> seen = new HashMap<>();
-        List<List<String>> result = new ArrayList<>();
-
-        for (String s : strs) {
-           seen.computeIfAbsent(hashCode(s), k->new ArrayList<>()).add(s);
+        for(String s : strs){
+            int[] count = new int[26];
+            for(char c : s.toCharArray()){
+                count[c - 'a'] += 1;
+            }
+            String key = Arrays.toString(count);
+            seen.putIfAbsent(key, new ArrayList<>());
+            seen.get(key).add(s);
         }
-
-        for(List<String> e : seen.values()){
-            result.add(e);
-        }
-
-        return result;
-
-    }
-
-    public String hashCode(String word) {
-        int[] nums = new int[26];
-        for (int i = 0; i < word.length(); i++) {
-            char current = word.charAt(i);
-            int position = current - 'a';
-            nums[position] += 1;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        char current = 'a';
-        for (int i : nums) {
-            sb.append(current + "" + i);
-            current += 1;
-        }
-
-        return sb.toString();
+        return new ArrayList<>(seen.values());
     }
 }
